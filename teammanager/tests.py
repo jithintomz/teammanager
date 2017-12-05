e95c574e0c50e9826bbb7248b72a9dfe79d6552d
root <root@PC06EE6R.localdomain>
2017-12-05 20:37:20 +0530

    Added basic tests

--- teammanager/tests.py
+++ teammanager/tests.py
@@ -1,3 +1,17 @@
-from django.test import TestCase
 
-# Create your tests here.
+from rest_framework.test import APIClient
+import unittest
+
+
+class Members(unittest.TestCase):
+	def test(self):
+		client = APIClient()
+		response = client.get('/api/members/')
+		self.assertEqual(response.status_code,200)
+		user_data = {"first_name": "Jane","last_name": "Doe","phoneno": "8281958680",
+		"email": "janedoe1@gmail.com","role": "Regular"}
+		response = client.post('/api/members/', user_data, format='json')
+		self.assertEqual(response.status_code,201)
+		user_data["phoneno"] = "+918281958680"
+		response = client.patch('/api/members/1/', user_data, format='json')
+		self.assertEqual(response.status_code,200)
